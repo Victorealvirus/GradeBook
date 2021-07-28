eca6975a7f71ab94fc9a9d9dd9599aa4a90f7df9
adwait.nadkarni@centralsquare.com <adwait.nadkarni@centralsquare.com>
2021-07-28 13:27:33 -0700

    Added encapsulation

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Book
+    {
+        List<double> grades = new List<double>();
+        string name;
+
+        public Book(string name)
+        {
+            this.name = name;
+        }
+        public void AddGrades(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {            
+            Console.WriteLine($"The highest grade in {name} is {grades.Max()}");
+            Console.WriteLine($"The lowest grade in {name} is {grades.Min()}");
+            Console.WriteLine($"The average grade in {name} is {grades.Average()}");
+        }
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,24 +7,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 }; 
-            grades.Add(56.1);
-            
-            var result = 0.0;
-            foreach (var number in grades)
-            { 
-                result += number;
-            }
-            result /= grades.Count;
+            var book = new Book("Adwait's grade book");
+            book.AddGrades(89.1);
+            book.AddGrades(90.5);
+            book.AddGrades(77.5);
 
-            Console.WriteLine($"The average grade is {result:N2}");
-
-            if (args.Length > 0)
-                Console.WriteLine($"Hello, {args[0]}!");
-
-            else
-                Console.WriteLine("Hello World!");
-
+            book.ShowStats();
         }
     }
 }
